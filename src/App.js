e81357372b89f6f6665fa934995910b9d6cdc9cd
Tuomas <n9katu00@students.oamk.fi>
2021-03-20 00:08:34 +0200

    melkein valmis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,186 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+
+const URL = 'http://localhost/TODO/';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [items, setItems] = useState([]);
+    const [item, setItem] = useState('');
+    const [amount, setAmount] = useState('');
+    const [editItem, setEditItem] = useState(null);
+    const [editAmount, setEditAmount] = useState('')
+    const [editDescription, setEditDescription] = useState('');
+  
+    useEffect(() => {
+      let status = 0;
+      fetch(URL + 'index.php')
+      
+      .then(response => {
+        status = parseInt(res.status);
+        return res.json();
+      })
+      .then(
+        (res) => {
+          if(status === 200) {
+          setItems(res);
+        } else {
+          alert(res.error);
+        }
+        }, (error) => {
+          alert(error)
+        }
+      )
+    },
+  
+    function save(e) {
+      e.preventDefault();
+  
+      let status = 0;
+      fetch(URL + 'create.php',{
+        method: 'POST',
+        headers: {
+          'Accept': 'application(json)',
+          'Content-type':'application(json)',
+        },
+        body: JSON.stringify({
+          description: item,
+          amount: amount
+        })
+      })
+      .then(res => {
+        status = parseInt(res.status);
+        return res.json();
+      })
+      .then(
+        (res) => {
+          if(status ===200) {
+            setItems(items => [...items,res]);
+            setItem('');
+            setAmount('');
+          } else {
+            alert(res.error);
+          }
+          }, (error) => {
+            alert(error)
+          }
+      )
+    },
+  
+    function remove(id) {
+      let status = 0;
+      fetch(URL + 'delete.php',{
+        method: 'POST',
+        headers: {
+          'Accept': 'application(json)',
+          'Content-type':'application(json)',
+        },
+        body: JSON.stringify({
+          id: id
+        })
+      })
+      .then(res => {
+        status = parseInt(res.status);
+        return res.json();
+      })
+      .then(
+        (res) => {
+          if(status ===200) {
+            const newListWithoutRemoved = items.filter((item) => item.id !== id);
+            setItems(newListWithoutRemoved);
+          } else {
+            alert(res.error);
+          }
+          }, (error) => {
+            alert(error)
+          }
+      )
+    },
+  
+    function setEditedItem(item) {
+      setEditItem(item);
+      setEditDescription(item?.description);
+      setEditAmount(item?.amount);
+    },
+  
+    function update(e) {
+      e.preventDefault();
+      let status = 0;
+      fetch(URL + 'update.php',{
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-type':'application/json',
+        },
+        body: JSON.stringify({
+          id: editItem.id,
+          description: editDescription,
+          amount: editAmount
+        })
+      })
+      .then(res => {
+        status = parseInt(res.status);
+        return res.json();
+      })
+      .then(
+        (res) => {
+          if(status ===200) {
+            items[(items.findIndex(item => item.id === editItem.id))].description = editDescription;
+            items[(items.findIndex(item => item.id === editItem.id))].amount = editAmount;
+            setItems([...items]);
+            setEditItem(null);
+            setEditDescription('');
+            setEditAmount('');
+          } else {
+            alert(res.error);
+          }
+          }, (error) => {
+            alert(error)
+          }
+      )
+    }, [])
+  
+    return (
+      <>
+        <div className="container">
+          <h3>Shopping list</h3>
+          <form onSubmit={save}>
+            <div>
+              <label>New item</label>&nbsp;
+              <input value={item} onChange={e=> setItem(e.target.value)}></input>
+            </div>
+            <div>
+              <label>Amount</label>&nbsp;
+              <input value={amount} onChange={e=> setAmount(e.target.value)}></input>
+            </div>
+            <button>Save</button>
+          </form>
+          <ol>
+            {items.map(item => (
+              <li key={item.id}>
+                {editItem?.id !== item.id &&
+                item.description
+                } &nbsp;   
+                {editItem?.id !== item.id &&
+                  item.amount
+                } 
+                {editItem?.id === item.id &&
+                  <form onSubmit={update}>
+                    <input value={editDescription} onChange={e=> setEditDescription(e.target.value)} />
+                    <input value={editAmount} onChange={e=> setEditAmount(e.target.value)} />
+                    <button>Save</button>
+                    <button type="button" onClick={() => setEditedItem(null)}>Cancel</button>
+                  </form>
+                } &nbsp; &nbsp;
+                <a className="delete" onClick={() => remove(item.id)} href="#">Delete</a>&nbsp;
+                {editItem === null &&
+                <a className="edit" onClick={() => setEditedItem(item)} href="#">Edit</a>
+                } 
+              </li>
+            ))}
+          </ol>
+        </div>
+      </>
+    );
 }
 
 export default App;
